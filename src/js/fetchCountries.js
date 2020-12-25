805506e72de9a1dd00b21995dd50a35d468e836a
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-12-25 15:59:05 +0200

    adds first var of hw

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,31 +2,28 @@ import API from './api-service.js';
 import getRefs from './get-refs';
 import countriesTpl from '../templates/countries-list.hbs';
 import countryTpl from '../templates/country-card.hbs';
+
 import '@pnotify/core/dist/BrightTheme.css';
 import { defaults } from '@pnotify/core';
+const { error } = require('@pnotify/core');
+
 defaults.width = '250px';
 defaults.minHeight = '35px';
-defaults.delay = '1000';
+defaults.delay = '500';
 defaults.closer = false;
 defaults.sticker = false;
-defaults.addClass = 'error';
-defaults.autoOpen = false; 
 
 
+const debounce = require('lodash.debounce');
 const refs = getRefs();
-refs.searchForm.addEventListener('submit', onSearch);
-// refs.searchForm.addEventListener('input', debounce(onSearch, 500));
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
-function onSearch(e) {
-  e.preventDefault();
+function onSearch(evt) {
+  evt.preventDefault();
+  refs.cardContainer.innerHTML = '';
 
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
-
-  API.fetchCountries(searchQuery)
-    .then(renderedCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
+  const searchQuery = evt.target.value;
+  API.fetchCountries(searchQuery).then(renderedCard).catch(onFetchError);
 }
 function renderedCard(countries) {
   if (countries.length > 10) {
@@ -34,12 +31,11 @@ function renderedCard(countries) {
   }
 
   if (countries.length === 1) {
-    renderCountryCard(countries[0]);
-    return;
+    return renderCountryCard(countries[0]);
   }
 
   if (countries.length <= 10 || countries.length > 1) {
-    renderCountryListCard(countries);
+    return renderCountryListCard(countries);
   }
 }
 
@@ -53,7 +49,9 @@ function renderCountryListCard(countries) {
 }
 
 
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так и мы не нашли такую страну!');
+function onFetchError() {
+  error({
+    text: 'Упс, что-то пошло не так и мы нашли слишком много совпадений!',
+  });
 }
 
